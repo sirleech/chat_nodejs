ad5b586f5db467bafb5d1ef62569d09658509aeb
chris <sirleech@gmail.com>
2012-03-07 15:34:17 +1100

    remove copied code

--- chatClient.js
+++ chatClient.js
@@ -1,34 +1,3 @@
-
-// REMOVE THESE!!!!
-////////////////////////////
-function print2dArray(grid) {
-	$("#arrayPrint").empty();			
-	for (var y = 0; y < grid[0].length; y++){
-		for (var x = 0; x < grid.length; x++){			
-			$("#arrayPrint").append(grid[x][y] + "   ");
-		}
-		$("#arrayPrint").append("<br/></br>");
-	}
-}
-
-// create a 2d array 
-// array[height][width]
-
-function createMultiArray(x,y) {
-	var grid = new Array(x);
-
-	for (var i = 0; i < x; i++) {
-		grid[i] = new Array(y);
-		for (var k = 0; k < y; k++) {
-			grid[i][k] = 0;
-		}
-	}
-	return grid;
-}
-
-
-///////////////////////////////////
-
 // t = timeout
 var t;
 var circles = createMultiArray(5,5);
